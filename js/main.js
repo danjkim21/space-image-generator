3f83c6685b449cf268f1753d9dc7cb1ce05d5b1b
danjkim21 <dan.jkim21@gmail.com>
2022-05-14 17:20:59 -0400

    updated mobile responsiveness and added new link to HD images

--- js/main.js
+++ js/main.js
@@ -4,58 +4,64 @@
 document.querySelector('button').addEventListener('click', getPictureOfTheDay);
 
 function getPictureOfTheDay() {
-    // set date to user selected
-    let date = document.querySelector('input').value
+  // set date to user selected
+  let date = document.querySelector('input').value;
 
-    // requesting data from a server via a URL API and specifying specific image date in the url
+  // requesting data from a server via a URL API and specifying specific image date in the url
 
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=1a3gGVcYGA19BfITQHuUIjIz00FnSpe12dqV3EKh&date=${date}`) 
-        // parse response as JSON 
-        .then(res => res.json()) 
+  fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=1a3gGVcYGA19BfITQHuUIjIz00FnSpe12dqV3EKh&date=${date}`
+  )
+    // parse response as JSON
+    .then((res) => res.json())
 
-        .then(data => {
-            // console log the object
-            console.log(data)
+    .then((data) => {
+      // console log the object
+      console.log(data);
 
-            // Conditional if there is a image title
-            if (!data.title) {
-                document.querySelector('h1').innerHTML = "";
-            } else {
-                // change h2 to title of image
-                document.querySelector('h1').innerHTML = data.title;
-                // add image title to description title
-                document.querySelector('.imgTitle').innerHTML = data.title;
-            }
+      // Conditional if there is a image title
+      if (!data.title) {
+        document.querySelector('h1').innerHTML = '';
+      } else {
+        // change h2 to title of image
+        document.querySelector('h1').innerHTML = data.title;
+        // add image title to description title
+        document.querySelector('.imgTitle').innerHTML = data.title;
+      }
 
-            // add date to explanation 
-            document.querySelector('.dateHere').innerHTML = data.date;
-            // add description to explanation
-            document.querySelector('.explanation').innerHTML = data.explanation;
-            
-            // Conditional if there is a copyright person
-            if (!data.copyright) {
-                document.querySelector('.copyright').innerHTML = "";
-            } else {
-                // add copyright description to copyright
-                document.querySelector('.copyright').innerHTML = data.copyright;
-            }
+      // add date to explanation
+      document.querySelector('.dateHere').innerHTML = data.date;
+      // add description to explanation
+      document.querySelector('.explanation').innerHTML = data.explanation;
+      // add link to href of full image button
+      document.querySelector('.getImgBtn').href = data.hdurl;
 
-            // Figure out how to control for videos see date 2021-02-03: use conditional and use iframe in html
-            if (data.media_type === 'image') {
-                // document.getElementById("myDiv").style.backgroundImage = "url('img_tree.png')";
-                document.querySelector('main').style.backgroundImage = `url('${data.hdurl}')`;
-                // document.querySelector('img').src = data.hdurl;
-                // document.querySelector('iframe').src = '';
-            } else if (data.media_type === 'video') {
-                document.querySelector('iframe').src = data.url;
-                document.querySelector('img').src = '';
-                document.querySelector('iframe').classList.remove('hidden');
-            }
+      // Conditional if there is a copyright person
+      if (!data.copyright) {
+        document.querySelector('.copyright').innerHTML = '';
+      } else {
+        // add copyright description to copyright
+        document.querySelector('.copyright').innerHTML = data.copyright;
+      }
 
-            document.querySelector('.description').classList.remove('hidden');
-            document.querySelector('.teaser').classList.remove('hidden');
-        })
+      // Figure out how to control for videos see date 2021-02-03: use conditional and use iframe in html
+      if (data.media_type === 'image') {
+        // document.getElementById("myDiv").style.backgroundImage = "url('img_tree.png')";
+        document.querySelector(
+          'main'
+        ).style.backgroundImage = `url('${data.url}')`;
+        // document.querySelector('img').src = data.hdurl;
+        // document.querySelector('iframe').src = '';
+      } else if (data.media_type === 'video') {
+        document.querySelector('iframe').src = data.url;
+        document.querySelector('img').src = '';
+        document.querySelector('iframe').classList.remove('hidden');
+      }
 
-        // promise 
-        .catch(err => console.log('error ${err}'));
+      document.querySelector('.description').classList.remove('hidden');
+      document.querySelector('.teaser').classList.remove('hidden');
+    })
+
+    // promise
+    .catch((err) => console.log('error ${err}'));
 }
